604ae325bb5882cce9036f241c0211cd1e937d0a
luthebao <luthebao97@gmail.com>
2024-02-01 05:22:47 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,9 @@ export default function Document() {
                 <meta charSet="utf-8" />
                 <link rel="icon" href="/logo.png" />
                 <meta name="theme-color" content="#000000" />
-                <meta name="description" content="Web site created using create-react-app" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-                <link rel="stylesheet" href="assets/fonts/Genotics/stylesheet.css" />
+                {/* <meta name="description" content="Web site created using create-react-app" /> */}
+                {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" /> */}
+                {/* <link rel="stylesheet" href="assets/fonts/Genotics/stylesheet.css" /> */}
             </Head>
             <body>
                 <Main />
